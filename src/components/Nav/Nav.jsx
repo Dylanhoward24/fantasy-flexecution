7d63434639b3b0d484ea15547b930e55f9f7a7ce
Dylan Howard <dylanhoward0424@outlook.com>
2021-09-07 18:27:45 -0500

    added styling

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -9,20 +9,47 @@ function Nav() {
 
   return (
     <div className="header">
-      <Link to="/home">
-        <h1 className="header-title">Fantasy Flexecution</h1>
-      </Link>
-      <div>
-          <Link className="headerLink" to="/login">
-            Login
-          </Link>
-          <Link className="headerLink" to="/registration">
-            Register
-          </Link>
-          <img className="logo" src="https://m.media-amazon.com/images/I/51zTw14COAL._SL500_.jpg" />
+
+      <div className="upperHeader">
+        <Link to="/home">
+          <h1 className="header-title">Fantasy Flexecution</h1>
+        </Link>
+        <div>
+            <img className="logo" src="https://m.media-amazon.com/images/I/51zTw14COAL._SL500_.jpg" />
+        </div>
       </div>
+
+      <div className="lowerHeader">
+        <div className="lowerLeftHeader">
+          <Link className="navLink" to="/home">
+            <p>Home</p>
+          </Link>
+          <Link className="navLink" to="/overallranks">
+            <p>Rankings</p>
+          </Link>
+          <Link className="navLink" to="/podcasts">
+            <p>Podcasts</p>
+          </Link>
+        </div>
+        <div className="lowerRightHeader">
+          <Link className="navLink" to="/login">
+            <p>Login</p>
+          </Link>
+          <Link className="navLink" to="/registration">
+            <p>Register</p>
+          </Link>
+        </div>
+      </div>
+
     </div>
   );
 }
 
 export default Nav;
+
+{/* <Link className="headerLink" to="/login">
+              Login
+            </Link>
+            <Link className="headerLink" to="/registration">
+              Register
+            </Link> */}
