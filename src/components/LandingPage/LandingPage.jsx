9ee66a04edb49269ca098650ffcec74d8bab8498
Dylan Howard <dylanhoward0424@outlook.com>
2021-09-08 09:52:26 -0500

    putting pause on landing page for now, going to setup auth

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,26 +1,32 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import './LandingPage.css';
 
-// CUSTOM COMPONENTS
-import RegisterForm from '../RegisterForm/RegisterForm';
-
-function LandingPage() {
-  const [heading, setHeading] = useState('Welcome');
+export default function LandingPage() {
   const history = useHistory();
+  const dispatch = useDispatch();
 
-  const onLogin = (event) => {
-    history.push('/login');
-  };
+  let [requestInfo, setRequestInfo] = useState('');
+
+  function submitRequest() {
+    
+  }
 
   return (
     <div className="container">
       <div className="grid-container">
         <div id="item1">
           <h3>Recent Podcasts</h3>
+          {}
         </div>
         <div id="item2">
           <h3>What do you want to hear on our show?</h3>
+          <textarea value={requestInfo} rows="8" cols="50"
+            placeholder="Submit your request here. We'll
+            address it in a future show!"
+            onChange={(e) => setRequestInfo(e.target.value)}/>
+          <button id="submitBtn" onClick={submitRequest}>Submit</button>
         </div>
         <div id="item3">
           <h3>Coming Up</h3>
@@ -29,5 +35,3 @@ function LandingPage() {
     </div>
   );
 }
-
-export default LandingPage;
--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -26,7 +26,7 @@ function LoginForm() {
 
   return (
     <>
-      <p>Under Constrution</p>
+      <p>Under Construction</p>
     </>
   );
 }
