8169ff973299fe3946ef2b6be6c4ecdecc067b43
Dylan Howard <dylanhoward0424@outlook.com>
2021-09-08 00:05:40 -0500

    added grid and sections to landing page

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -15,8 +15,17 @@ function LandingPage() {
 
   return (
     <div className="container">
-      <h2>Recent Podcasts</h2>
-
+      <div className="grid-container">
+        <div id="item1">
+          <h3>Recent Podcasts</h3>
+        </div>
+        <div id="item2">
+          <h3>What do you want to hear on our show?</h3>
+        </div>
+        <div id="item3">
+          <h3>Coming Up</h3>
+        </div>
+      </div>
     </div>
   );
 }
