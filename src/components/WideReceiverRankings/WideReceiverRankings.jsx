dcdc70565155b3e5b95e7ed101e2c5c8974402c3
Dylan Howard <dylanhoward0424@outlook.com>
2021-09-18 10:04:33 -0500

    finished with wrRankings component

--- src/components/WideReceiverRankings/WideReceiverRankings.jsx
+++ src/components/WideReceiverRankings/WideReceiverRankings.jsx
@@ -1,13 +1,47 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import RankingsHeader from '../RankingsHeader/RankingsHeader';
 import './WideReceiverRankings.css';
 
 export default function WideReceiverRankings() {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch({ type: 'FETCH_WIDERECEIVERS' });
+    }, []);
+
+    // global state
+    const hosts = useSelector((store) => store.hosts);
+    const wideReceivers = useSelector((store) => store.wideReceivers);
+
     return (
         <div className="container">
             <h1>Wide Receiver Rankings</h1>
             <RankingsHeader />
-
-            <p>WideReceiverRankings component under construction...</p>
+            <center>
+                <table>
+                    <thead>
+                    <tr>
+                        <th className="playerNameTableDisplay">Player Name</th>
+                        <th className="teamTableDisplay">Team</th>
+                        {hosts.map((host, i) => (
+                            <th key={i}>Tier <br /> ({host.first_name})</th>
+                        ))}
+                        <th className="tagsTableDisplay">Tags</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {wideReceivers.map((receiver, i) => (
+                        <tr key={i}>
+                            <td>{receiver.firstName} {receiver.lastName}</td>
+                            <td>{receiver.team}</td>
+                            {receiver.tierName.map((tierName, i) => (
+                                <td key={i}>{tierName}</td>
+                            ))}
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </center>
         </div>
     );
 }
--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -13,6 +13,7 @@ import hostsSaga from './hosts.saga';
 import playersSaga from './players.saga';
 import quarterbacksSaga from './quarterbacks.saga';
 import runningBacksSaga from './runningBacks.saga';
+import wideReceiversSaga from './wideReceivers.saga';
  
 // rootSaga is the primary saga.
 // It bundles up all of the other sagas so our project can use them.
@@ -37,5 +38,6 @@ export default function* rootSaga() {
    playersSaga(),
    quarterbacksSaga(),
    runningBacksSaga(),
+   wideReceiversSaga(),
  ]);
 }
